df8e873eb123421cc6c3f944714151e6825c396c
munhoz <munhozfontana@gmail.com>
2021-07-06 13:22:23 -0300

    teste

--- lib/functions.dart
+++ lib/functions.dart
@@ -2,4 +2,4 @@ import 'package:functions_framework/functions_framework.dart';
 import 'package:shelf/shelf.dart';
 
 @CloudFunction()
-Response function(Request request) => Response.ok('Hello, World! do K8S');
+Response function(Request request) => Response.ok('Hello, World! do K8S 2');
